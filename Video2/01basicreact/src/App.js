0db1c2440cee872b0b75fae998f498bc6115a9a0
gautamKumar <gautamkannoujiya58@gmail.com>
2024-05-24 13:45:54 +0530

    created a basic react project with vite and react app

---
+++ Video2/01basicreact/src/App.js
@@ -0,0 +1,41 @@
+function App() {
+  return (
+    <>
+      <h1>Chai aur React | Gautam Kumar</h1>☑️Video 2: Create React ⚛️ Projects{" "}
+      <br />
+      🔻React code resources
+      <ul>
+        ↪️one code Editor, NodeJS environment (interpreter) for JS. ↪️React
+        documentation react.dev
+      </ul>
+      <br />
+      🔻React as Library
+      <br />
+      🔻React library has two attachments
+      <ul>
+        1. react-DOM 2. react-native ↪️Usability of above depends on what are
+        you building 1. react-DOM for website building 2. react-native for
+        building mobile apps
+      </ul>
+      <br />
+      🔻React Project
+      <br />
+      🔻Creating React Project with Vite
+      <ul>
+        <li>↪️Vite is a bundler.</li>
+        <li>
+          ↪️npm create vite@latest (npm is node package manager and create is
+          utility with latest vite)
+        </li>
+        <li>
+          ↪️Always start reading any react project starting with package.json.{" "}
+        </li>
+        <li>↪️Dependecies of Vite project is react and react-dom.</li>
+        <li>↪️npm install</li>
+        <li>↪️npm run dev (by refering to scripts key in package.json file)</li>
+      </ul>
+    </>
+  );
+}
+
+export default App;
